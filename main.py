3b572f311b746de4e79011bfa1de1f8f0a907a36
Za-SrankA <www.luckytim-16@inbox.ru>
2022-11-23 22:41:44 +0300

    commit_Ex6

--- main.py
+++ main.py
@@ -2,8 +2,6 @@
 import requests
 
 response = requests.get("https://playground.learnqa.ru/api/long_redirect", allow_redirects=True)
-first_response = response.history[0]
-second_response = response
 
-print(first_response.url)
-print(second_response.url)
+print(f"Кол-во редиректов: {len(response.history)}")
+print(f"Последний URL: {response.url}")
